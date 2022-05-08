1e7297668ab59ba352efaadbd52ff96356fb3b53
davidcoats <david@getsafetycone.com>
2022-05-08 15:37:47 -0700

    Initial commit of source-stub.

---
+++ source/R5T.S0034.Lib/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0034.Lib
+{
+    /// <summary>
+    /// Generation, initial, parse scripts for Roslyn syntax elements.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
